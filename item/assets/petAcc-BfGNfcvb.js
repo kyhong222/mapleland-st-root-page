23986134d799ef9a2dfbee6d80f83f54c9ff8e5f
kyhong222 <kyhong222@users.noreply.github.com>
2026-02-10 11:41:19 +0000

    deploy: 90c7803247e0ffa1f5b7e79c55c6685029abdbf8

---
+++ item/assets/petAcc-BfGNfcvb.js
@@ -0,0 +1,1 @@
+const e=[{id:1802e3,name:"Red Ribbon",koreanName:"투명한 리본",reqJob:0,reqLevel:0}];export{e as default};
---
+++ item/assets/petAcc-FgqGHbL_.js
@@ -0,0 +1,1 @@
+const t={1802e3:{koreanName:"투명한 리본",icon:"https://maplestory.io/api/GMS/200/item/1802000/icon",stats:{attack:0,str:0,dex:0,int:0,luk:0,mad:0,pdef:0,mdef:0,acc:0,eva:0},requireStats:{level:0,str:0,dex:0,int:0,luk:0}}};export{t as default};
